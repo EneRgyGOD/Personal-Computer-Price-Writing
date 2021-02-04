4706c75281da7f74854abd84041599aa98270c7b
EneRgy_GOD <48410107+EneRgyGOD@users.noreply.github.com>
2021-02-04 22:38:03 +0200

    Initial commit

---
+++ PCPW/Data.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PCPW
+{
+    class Data
+    {
+        public List<int> Price = new List<int>();
+        public List<string> Name = new List<string>();
+    }
+}
---
+++ PCPW/Parser.cs
@@ -0,0 +1,44 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Text;
+using System.Threading.Tasks;
+
+namespace PCPW
+{
+    class Parser
+    {
+        public async Task<Data> ParserAsync()
+        {
+            Data data = new Data();
+
+            var config = Configuration.Default.WithDefaultLoader();
+            var address = "https://ek.ua/ul_/fswtud0jhz1/zakladki/";
+            var document = await BrowsingContext.New(config).OpenAsync(address);
+            var contentPrice = document.QuerySelectorAll("td.model-hot-prices-td [id^=price], [class$=ib] span:first-child");
+            var contentName = document.QuerySelectorAll("td.model-short-info table span.u");
+
+            for (int i = 0; i < contentName.Length && i < contentPrice.Length; i++)
+            {
+                string check = contentPrice[i].Text();
+                string result = null;
+
+                for (int z = 0; !check[1].IsDigit() && z < check.Length; z++)
+                {
+                    if (z != 1)
+                    {
+                        result += check[z];
+                    }
+                }
+                if (check[1].IsDigit())
+                {
+                    result = check;
+                }
+
+                data.Price.Add(int.Parse(result));
+                data.Name.Add(contentName[i].Text());
+
+            }
+            return data;
+        }
+    }
+}
